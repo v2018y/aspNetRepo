468ec435394cd330a3e56e0817739af7838661a0
unknown <you@example.com>
2019-08-07 16:22:55 +0530

    0.1.4 : Added successfully User PUT & DELETE Method

--- CMDAPI/Controllers/UserController.cs
+++ CMDAPI/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CMDAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CMDAPI.Controllers
 {
@@ -37,13 +38,33 @@ namespace CMDAPI.Controllers
         [HttpPost]
         public ActionResult<User> PostUserItem(User user){
             _context.User.Add(user);
-           int i= _context.SaveChanges();
-            if(i>0){
-                return user;
-            }else{
+            _context.SaveChanges();
+           return CreatedAtAction("GetUserItem",new User{id=user.id},user);
+       }
+
+        //PUT : api/user/n
+        [HttpPut("{id}")]
+        public ActionResult PutUserItem(int id,User user){
+            if(id !=user.id){
+                return BadRequest();
+            }
+            _context.Entry(user).State= EntityState.Modified;
+            _context.SaveChanges();
+            return CreatedAtAction("GetUserItem",new User{id=user.id},user);
+        }
+        //DELETE : api/user/n
+        [HttpDelete("{id}")]
+        public ActionResult<User> DeleteUserItem(int id){
+            var userItem=_context.User.Find(id);
+            if(userItem == null){
                 return NotFound();
             }
+            _context.User.Remove(userItem);
+            _context.SaveChanges();
 
+            return userItem;
         }
+
+
     }
 }

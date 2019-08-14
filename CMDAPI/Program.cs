f7d31c11565c074623b152d8f80bacba88726101
unknown <you@example.com>
2019-08-14 10:50:10 +0530

    0.1.16 : Added  The UserId fileds all Table

--- CMDAPI/Program.cs
+++ CMDAPI/Program.cs
@@ -19,6 +19,7 @@ namespace CMDAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>().UseUrls("http://10.10.10.178:5001/");
+                .UseStartup<Startup>();
+                // .UseUrls("http://10.10.10.178:5001/");
     }
 }
